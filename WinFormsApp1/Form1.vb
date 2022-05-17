20b2ddaf6226056c9c73be66920765229b44abc7
Karen Payne <paynekaren@comcast.net>
2022-05-17 12:39:52 -0700

    vb.net file count

---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO
+Public Class Form1
+    Public Async Function FileCount(sender As String, allowedExtensions() As String) As Task(Of Integer)
+        Return Await Task.Run(
+            Function()
+                Return Task.FromResult(
+                    Directory.EnumerateFiles(
+                        sender, 
+                        "*.*", 
+                        SearchOption.AllDirectories).Count(
+                            Function (file)
+                                Return allowedExtensions.Contains(Path.GetExtension(file))
+                            End Function))
+            End Function)
+    End Function
+
+    Private Async Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim watch As Stopwatch = Stopwatch.StartNew()
+        Dim count = Await FileCount("C:\OED\Dotnetland\VS2019", { ".cs" })
+        watch.Stop()
+        Dim ts As TimeSpan = TimeSpan.FromMilliseconds(watch.Elapsed.TotalMilliseconds)
+
+        MessageBox.Show($"File count: {count,-10}{ts.TotalSeconds}")
+
+    End Sub
+End Class
---
+++ WinFormsApp1/readme.md
@@ -0,0 +1,19 @@
+﻿# About
+
+Get count of files in a folder and all sub-folders.
+
+```vbnet
+Public Async Function FileCount(sender As String, allowedExtensions() As String) As Task(Of Integer)
+    Return Await Task.Run(
+        Function()
+            Return Task.FromResult(
+                Directory.EnumerateFiles(
+                    sender, 
+                    "*.*", 
+                    SearchOption.AllDirectories).Count(
+                        Function (file)
+                            Return allowedExtensions.Contains(Path.GetExtension(file))
+                        End Function))
+        End Function)
+End Function
+```
