c805522dfdeec7d438a684a2ffb6179718297cc4
Karen Payne <paynekaren@comcast.net>
2023-01-18 08:18:23 -0800

    updated several projects to .net core 7

--- WindowsFormsLibrary/Classes/Dialogs.cs
+++ WindowsFormsLibrary/Classes/Dialogs.cs
@@ -11,6 +11,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using WindowsFormsLibrary.LanguageExtensions;
 using static System.DateTime;
+using Timer = System.Windows.Forms.Timer;
 
 
 namespace WindowsFormsLibrary.Classes
