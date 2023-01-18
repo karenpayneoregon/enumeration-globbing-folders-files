6fe4b1d83f879bc18e92ae39e17ac1168353b0b1
Karen Payne <paynekaren@comcast.net>
2023-01-18 08:31:22 -0800

    recreated vb project

--- EnumerateFoldersProject/ListViewForm.cs
+++ EnumerateFoldersProject/ListViewForm.cs
@@ -1,15 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using DirectoryHelpersLibrary.Classes;
+﻿using DirectoryHelpersLibrary.Classes;
 using DirectoryHelpersLibrary.Models;
 using static DirectoryHelpersLibrary.Classes.DirectoryOperations2;
 using static WindowsFormsLibrary.LanguageExtensions.ControlHelper;
