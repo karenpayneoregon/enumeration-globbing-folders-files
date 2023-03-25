c7d37aca215d6157b726144f40e86c8893f069ce
Karen Payne <paynekaren@comcast.net>
2023-03-25 09:23:36 -0700

    new project

---
+++ DeleteFolders/Classes/RemoveDirectoryOperations.cs
@@ -0,0 +1,125 @@
+﻿
+using System.Runtime.CompilerServices;
+
+namespace DeleteFolders.Classes;
+
+public class RemoveDirectoryOperations
+{
+    public delegate void OnDelete(string status, [CallerMemberName] string callerName = null);
+    /// <summary>
+    /// Callback for subscribers to see what is being worked on
+    /// </summary>
+    public static event OnDelete OnDeleteEvent;
+
+    public delegate void OnException(Exception exception, [CallerMemberName] string callerName = null);
+    /// <summary>
+    /// Callback for subscribers to know about a problem
+    /// </summary>
+    public static event OnException OnExceptionEvent;
+
+    public delegate void OnUnauthorizedAccessException(string message, [CallerMemberName] string callerName = null);
+    /// <summary>
+    /// Raised when attempting to access a folder the user does not have permissions too
+    /// </summary>
+    public static event OnUnauthorizedAccessException UnauthorizedAccessEvent;
+
+    public delegate void OnTraverseExcludeFolder(string sender);
+    /// <summary>
+    /// Called each time a folder is being traversed
+    /// </summary>
+    public static event OnTraverseExcludeFolder OnTraverseIncludeFolderEvent;
+
+    public static bool Cancelled = false;
+
+    /// <summary>
+    /// Recursively remove an entire folder structure and files with events for monitoring and basic
+    /// exception handling. USE WITH CARE
+    /// </summary>
+    /// <param name="directoryInfo"></param>
+    /// <param name="cancellationToken"></param>
+    /// <param name="execute">true to perform delete, false not to perform delete</param>
+    public static async Task RecursiveDelete(DirectoryInfo directoryInfo, CancellationToken cancellationToken, bool execute  = false)
+    {
+        if (!directoryInfo.Exists)
+        {
+            OnDeleteEvent?.Invoke("Nothing to process");
+            return;
+        }
+
+        OnDeleteEvent?.Invoke(directoryInfo.Name);
+
+        DirectoryInfo folder = null;
+
+        try
+        {
+            await Task.Run(async () =>
+            {
+                foreach (DirectoryInfo dirInfo in directoryInfo.EnumerateDirectories())
+                {
+
+                    folder = dirInfo;
+
+                    if (
+                        (folder.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden || 
+                        (folder.Attributes & FileAttributes.System) == FileAttributes.System || 
+                        (folder.Attributes & FileAttributes.ReparsePoint) == FileAttributes.ReparsePoint) {
+
+                        OnTraverseIncludeFolderEvent?.Invoke($"Nope {folder.FullName}");
+
+                        continue;
+
+                    }
+
+                    OnTraverseIncludeFolderEvent?.Invoke($"Delete this folder: {folder.FullName}");
+
+                    if (!Cancelled)
+                    {
+                        await Task.Delay(1, cancellationToken);
+                        await RecursiveDelete(folder, cancellationToken);
+                    }
+                    else
+                    {
+                        return;
+                    }
+
+                    if (cancellationToken.IsCancellationRequested)
+                    {
+                        cancellationToken.ThrowIfCancellationRequested();
+                    }
+
+                }
+                
+                if (execute)
+                {
+                    try
+                    {
+                        directoryInfo.Delete(true);
+                    }
+                    catch (Exception exception)
+                    {
+                        OnExceptionEvent?.Invoke(exception);
+                    }
+                }
+
+                OnDeleteEvent?.Invoke($"Fake delete {directoryInfo.Name}");
+
+            }, cancellationToken);
+
+        }
+        catch (Exception exception)
+        {
+            switch (exception)
+            {
+                case OperationCanceledException _:
+                    Cancelled = true;
+                    break;
+                case UnauthorizedAccessException _:
+                    UnauthorizedAccessEvent?.Invoke($"Access denied '{exception.Message}'");
+                    break;
+                default:
+                    OnExceptionEvent?.Invoke(exception);
+                    break;
+            }
+        }
+    }
+}
---
+++ DeleteFolders/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Spectre.Console;
+using static DeleteFolders.Classes.RemoveDirectoryOperations;
+
+namespace DeleteFolders;
+
+internal partial class Program
+{
+    private static readonly CancellationTokenSource cancellationToken = new();
+    static async Task Main(string[] args)
+    {
+        DirectoryInfo info = new("C:\\Work");
+        AnsiConsole.MarkupLine($"[yellow]Traversing[/] {info.FullName}");
+
+        // setup listeners 
+        OnDeleteEvent += RemoveDirectoryOperations_OnDeleteEvent;
+        OnExceptionEvent += RemoveDirectoryOperations_OnExceptionEvent;
+        UnauthorizedAccessEvent += RemoveDirectoryOperations_UnauthorizedAccessEvent;
+        OnTraverseIncludeFolderEvent += RemoveDirectoryOperations_OnTraverseIncludeFolderEvent;
+
+        // perform mocked delete
+        await RecursiveDelete(info, cancellationToken.Token );
+
+        Console.ReadLine();
+    }
+
+    private static void RemoveDirectoryOperations_OnTraverseIncludeFolderEvent(string sender)
+    {
+        AnsiConsole.MarkupLine($"\t[white]{sender}[/]");
+    }
+
+    private static void RemoveDirectoryOperations_UnauthorizedAccessEvent(
+        string message, [CallerMemberName] string callerName = null)
+    {
+        AnsiConsole.MarkupLine($"[yellow on red]{callerName}[/] {message}");
+    }
+
+    private static void RemoveDirectoryOperations_OnExceptionEvent(
+        Exception exception, [CallerMemberName] string callerName = null)
+    {
+        AnsiConsole.MarkupLine($"[white on red]{callerName}[/] {exception.Message}");
+    }
+
+    private static void RemoveDirectoryOperations_OnDeleteEvent(
+        string status, [CallerMemberName] string callerName = null)
+    {
+        AnsiConsole.MarkupLine($"[cyan]{callerName}[/] {status}");
+    }
+}
